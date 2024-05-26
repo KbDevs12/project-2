29a375b0272c19691527cad8359c36ceb0c11ba9
KbDevs12 <daniaku203@gmail.com>
2024-05-26 18:41:07 +0700

    update index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FormDaftar from "@/components/FormDaftar/FormDaftar";
+import Waves from "@/components/Waves/Waves";
 import {
   Bookmarks,
   Globe,
@@ -183,13 +184,7 @@ export default function Home() {
           </div>
         </div>
         <div className="relative -bottom-1">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path
-              fill="#1e3a8a"
-              fillOpacity="1"
-              d="M0,32L30,32C60,32,120,32,180,48C240,64,300,96,360,117.3C420,139,480,149,540,170.7C600,192,660,224,720,218.7C780,213,840,171,900,170.7C960,171,1020,213,1080,202.7C1140,192,1200,128,1260,101.3C1320,75,1380,85,1410,90.7L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
-            ></path>
-          </svg>
+          <Waves />
         </div>
         <div className="text-center bg-blue-900 pt-0 pb-8 px-0">
           <h3 className="text-xl text-white font-bold mb-4">

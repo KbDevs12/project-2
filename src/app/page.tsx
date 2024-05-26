81a6721d045b9baed3fc6652718ab2e2358848db
KbDevs12 <daniaku203@gmail.com>
2024-05-26 17:25:07 +0700

    update index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import FormDaftar from "@/components/FormDaftar/FormDaftar";
+import {
+  Bookmarks,
+  Globe,
+  GraduationCap,
+} from "@phosphor-icons/react/dist/ssr";
 
 export default function Home() {
   return (
@@ -19,23 +24,22 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-14 px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 text-center gap-6 mt-14 px-4">
           <div
             data-aos="fade-down-right"
             data-aos-easing="ease-in-out"
             data-aos-delay="50"
-            className="card bg-gray-200 shadow-2xl rounded-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-3xl"
+            className="card bg-blue-400 shadow-2xl shadow-black rounded-lg overflow-hidden transform cursor-pointer transition-transform hover:scale-105 hover:shadow-3xl"
           >
-            <img
-              src="https://source.unsplash.com/random/800x600?campus"
-              alt="Keunggulan 1"
-              className="w-full h-48 object-cover"
-            />
+            <div className="flex items-center justify-center py-4">
+              <GraduationCap size={150} color="#030303" weight="duotone" />
+            </div>
             <div className="p-4">
-              <h3 className="text-xl font-bold mb-2">Keunggulan 1</h3>
-              <p className="text-gray-700">
-                Kampus kami memiliki fasilitas modern yang mendukung kegiatan
-                belajar mengajar.
+              <h3 className="text-xl font-bold mb-2">Lulusan Berprestasi</h3>
+              <p className="text-white">
+                Lulusan kami diakui sebagai yang terbaik, dengan banyak yang
+                berhasil dalam karir mereka dan memberikan kontribusi signifikan
+                di berbagai bidang.
               </p>
             </div>
           </div>
@@ -43,18 +47,18 @@ export default function Home() {
             data-aos="fade-down"
             data-aos-easing="ease-in-out"
             data-aos-delay="50"
-            className="card bg-gray-200 shadow-2xl rounded-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-3xl"
+            className="card bg-blue-400 shadow-2xl shadow-black cursor-pointer rounded-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-3xl"
           >
-            <img
-              src="https://source.unsplash.com/random/800x601?campus"
-              alt="Keunggulan 2"
-              className="w-full h-48 object-cover"
-            />
+            <div className="flex justify-center items-center py-4">
+              <Bookmarks size={150} weight="duotone" />
+            </div>
             <div className="p-4">
-              <h3 className="text-xl font-bold mb-2">Keunggulan 2</h3>
-              <p className="text-gray-700">
-                Kami menawarkan berbagai program studi yang dirancang untuk
-                mempersiapkan mahasiswa menghadapi tantangan global.
+              <h3 className="text-xl font-bold mb-2">Prodi Favorit</h3>
+              <p className="text-white">
+                Program studi favorit kami menawarkan kurikulum yang inovatif
+                dan relevan dengan kebutuhan industri, didukung oleh fasilitas
+                modern dan dosen berpengalaman. Bergabunglah dengan kami untuk
+                meraih masa depan yang gemilang.
               </p>
             </div>
           </div>
@@ -62,26 +66,25 @@ export default function Home() {
             data-aos="fade-down-left"
             data-aos-easing="ease-in-out"
             data-aos-delay="50"
-            className="card bg-gray-200 shadow-2xl rounded-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-3xl"
+            className="card bg-blue-400 shadow-2xl shadow-black cursor-pointer rounded-lg overflow-hidden transform transition-transform hover:scale-105 hover:shadow-3xl"
           >
-            <img
-              src="https://source.unsplash.com/random/800x602?campus"
-              alt="Keunggulan 3"
-              className="w-full h-48 object-cover"
-            />
+            <div className="flex justify-center items-center py-4">
+              <Globe size={150} color="#0f0f0f" weight="duotone" />
+            </div>
             <div className="p-4">
-              <h3 className="text-xl font-bold mb-2">Keunggulan 3</h3>
-              <p className="text-gray-700">
-                Jaringan luas dengan industri dan institusi akademik di seluruh
-                dunia.
+              <h3 className="text-xl font-bold mb-2">Jaringan Global</h3>
+              <p className="text-white">
+                Kami memiliki jaringan luas dengan berbagai industri dan
+                institusi akademik di seluruh dunia, memungkinkan kolaborasi
+                dalam penelitian dan pertukaran pelajar.
               </p>
             </div>
           </div>
         </div>
         <div className="profile-kampus mt-8 px-4 overflow-y-hidden">
-          <h2 className="text-2xl font-bold mb-4 text-center">PROGRAM STUDI</h2>
+          <h2 className="text-2xl font-bold my-4 text-center">PROGRAM STUDI</h2>
           <div className="flex flex-col gap-4 my-8">
-            <div className="flex flex-col sm:flex-row items-center sm:items-start">
+            <div className="flex flex-col sm:flex-row items-center sm:items-start overflow-y-hidden">
               <div
                 className="sm:order-2 sm:ml-4"
                 data-aos="zoom-in-right"
@@ -104,10 +107,16 @@ export default function Home() {
                 <p className="text-lg">
                   Program studi Teknik Informatika berfokus pada pengembangan
                   perangkat lunak, sistem komputer, dan teknologi informasi.
+                  Mahasiswa akan mempelajari berbagai bahasa pemrograman,
+                  algoritma, struktur data, serta teknik pengembangan perangkat
+                  lunak modern. Selain itu, mereka juga akan dibekali dengan
+                  pengetahuan tentang keamanan siber, kecerdasan buatan, dan
+                  komputasi awan, yang semuanya sangat relevan dengan kebutuhan
+                  industri saat ini.
                 </p>
               </div>
             </div>
-            <div className="flex flex-col sm:flex-row items-center sm:items-start">
+            <div className="flex flex-col sm:flex-row items-center sm:items-start overflow-y-hidden">
               <div
                 className="sm:order-1 sm:mr-4"
                 data-aos="zoom-in-left"
@@ -129,11 +138,17 @@ export default function Home() {
                 <h3 className="text-xl font-bold mb-2">Sistem Informasi</h3>
                 <p className="text-lg">
                   Program studi Sistem Informasi menggabungkan ilmu komputer
-                  dengan bisnis untuk mengelola dan menganalisis data.
+                  dengan bisnis untuk mengelola dan menganalisis data. Mahasiswa
+                  akan mempelajari bagaimana merancang, mengimplementasikan, dan
+                  mengelola sistem informasi yang mendukung operasi bisnis.
+                  Kurikulum mencakup topik-topik seperti analisis sistem,
+                  manajemen basis data, e-commerce, dan analitik bisnis, yang
+                  semuanya dirancang untuk mempersiapkan lulusan yang mampu
+                  mengintegrasikan teknologi informasi dengan strategi bisnis.
                 </p>
               </div>
             </div>
-            <div className="flex flex-col sm:flex-row items-center sm:items-start">
+            <div className="flex flex-col sm:flex-row items-center sm:items-start overflow-y-hidden">
               <div
                 className="sm:order-2 sm:ml-4"
                 data-aos="zoom-in-right"
@@ -156,7 +171,12 @@ export default function Home() {
                 <p className="text-lg">
                   Program studi Film, TV & Media mempersiapkan mahasiswa untuk
                   berkarir di industri kreatif dengan fokus pada produksi dan
-                  manajemen media.
+                  manajemen media. Mahasiswa akan mempelajari teknik produksi
+                  film dan televisi, termasuk penulisan naskah, penyutradaraan,
+                  sinematografi, dan editing. Selain itu, mereka juga akan
+                  mempelajari aspek-aspek manajemen media, seperti distribusi
+                  konten, pemasaran, dan manajemen proyek, yang semuanya penting
+                  untuk sukses di industri yang dinamis ini.
                 </p>
               </div>
             </div>
